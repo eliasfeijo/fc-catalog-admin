2df19987efa57c2865525e2c6f50bd77cb285aca
Elias Feijó de Almeida Pereira Lima <eliasfeijo.dev@gmail.com>
2023-06-26 05:57:05 +0000

    refactor: moving value objects

--- src/@shared/domain/value-objects/unique-entity-id.vo.ts
+++ src/@shared/domain/value-objects/unique-entity-id.vo.ts
@@ -1,4 +1,4 @@
-import InvalidUuidError from "../errors/invalid-uuid.error";
+import InvalidUuidError from "../../errors/invalid-uuid.error";
 import { v4 as uuid, validate as uuidValidate } from "uuid";
 
 export class UniqueEntityId {
